b39895c2c8af01c35510884976692145247dfa57
wanghao <1750110604@qq.com>
2020-10-15 14:56:02 +0800

    update

--- build/config.js
+++ build/config.js
@@ -1,3 +1,5 @@
+// 抽离css
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 /**
  * webpack 环境变量
  */
@@ -5,8 +7,17 @@ const webpackEnv = {
     isEnvDevelopment: process.env.NODE_ENV === 'development',
     isEnvProduction: process.env.NODE_ENV === 'production'
 };
+/**
+ * css 的基础loaders
+ */
+const cssBaseLoaders = [
+    webpackEnv.isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+    'css-loader',
+    'postcss-loader'
+]
 
 
 module.exports = {
-    webpackEnv
+    webpackEnv,
+    cssBaseLoaders
 }
--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,20 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// 抽离css
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // 引入tsconfig.json文件
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 // 速度分析
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 const smp = new SpeedMeasurePlugin();
-// 多进程打包
-const Happypack = require('happypack');
 // 打包进度条
 const WebpackBar = require('webpackbar');
 // antd主题文件
 const theme = require('../src/antd/theme');
 // 引入config配置
-const { webpackEnv } = require('./config');
+const { cssBaseLoaders } = require('./config');
 
 module.exports = smp.wrap({
     entry: path.join(__dirname, '../src/index.tsx'),
@@ -25,38 +21,28 @@ module.exports = smp.wrap({
     module: {
         rules: [{
             test: /\.(j|t)sx?$/,
-            // use: [{
-            //     loader: 'babel-loader'
-            // }],
-            use: 'Happypack/loader?id=js',
+            use: ['babel-loader'],
             include: path.join(__dirname, '../src'),
             exclude: /node_modules/
         }, {
-            test: /\.(scss|css)$/,
-            use: [
-                // MiniCssExtractPlugin.loader只能在production环境使用, 不能和style-loader同时存在
-                webpackEnv.isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-                'css-loader',
-                'postcss-loader',
-                'sass-loader'
-            ],
+            test: /\.css$/,
+            use: cssBaseLoaders,
+            include: path.join(__dirname, '../src'),
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: [...cssBaseLoaders, 'sass-loader'],
             include: path.join(__dirname, '../src'),
             exclude: /node_modules/
         }, {
             test: /\.less$/,
-            use: [
-                // MiniCssExtractPlugin.loader只能在production环境使用, 不能和style-loader同时存在
-                webpackEnv.isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-                'css-loader',
-                'postcss-loader',
-                {
-                    loader: 'less-loader',
-                    options: {
-                        javascriptEnabled: true,
-                        modifyVars: theme
-                    }
+            use: [...cssBaseLoaders, {
+                loader: 'less-loader',
+                options: {
+                    javascriptEnabled: true,
+                    modifyVars: theme
                 }
-            ],
+            }]
         }]
     },
     plugins: [
@@ -65,10 +51,6 @@ module.exports = smp.wrap({
             filename: 'index.html',
             template: 'public/index.html',
             inject: true
-        }),
-        new Happypack({
-            id: 'js',
-            loaders: ['babel-loader']
         })
     ],
     resolve: {
@@ -78,9 +60,5 @@ module.exports = smp.wrap({
                 configFile: './tsconfig.json'
             })
         ]
-        // alias: {
-        //     '@': resolve('../src'),
-        //     '@components': resolve('../src/components')
-        // }
     }
 })
