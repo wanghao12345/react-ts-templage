d11633b5f84bf6efbb846a8e374ada86637e5aeb
wanghao <1750110604@qq.com>
2020-10-26 16:41:13 +0800

    update

--- src/page/Home/style.ts
+++ src/page/Home/style.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
 export const HomeWrapper = styled.div`
-    width: 100%;
-    height: 500px;
+    width: 100px;
+    height: 200px;
+    background-color: blue;
+    border-radius: 5px;
+    margin: 100px;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
